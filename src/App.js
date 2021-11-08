7ab6312fec8986e7a6f8715485f2bbd50d5ae288
sasindu <sasindulakpriyafernando@gmail.com>
2021-11-08 23:51:40 +0530

    Change App.js for demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NaviBar from "./components/NaviBar";
+import Bar from "./module/Bar";
+import Hr from "./module/HR";
+import Inquriy from "./module/Inquriy";
+import Room from "./module/Room";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NaviBar />
+      <Hr />
+      <Inquriy />
+      <Bar />
+      <Room />
+    </>
   );
 }
 
