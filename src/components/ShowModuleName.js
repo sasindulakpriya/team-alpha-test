a801984d4fb8afd2234894b0e27c674f5e7203a2
sasindu <sasindulakpriyafernando@gmail.com>
2021-11-08 23:50:38 +0530

    Add show module name component

---
+++ src/components/ShowModuleName.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ShowModuleName = (props) => {
+
+
+  return (
+    <div className="module-name">
+      <h1>{props.name}</h1>
+      <p>Module</p>
+    </div>
+  );
+};
+
+export default ShowModuleName;
